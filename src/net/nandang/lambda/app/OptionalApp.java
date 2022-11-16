d5a00df7fbf92b094b74d833ad7cbfcd5e9ae311
nan <net.nandang@gmail.com>
2022-11-16 14:45:10 +0700

    lambda - lambda on Optinal with method references

--- src/net/nandang/lambda/app/OptionalApp.java
+++ src/net/nandang/lambda/app/OptionalApp.java
@@ -2,6 +2,7 @@ package net.nandang.lambda.app;
 
 import java.util.Optional;
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class OptionalApp {
     public static void main(String[] args) {
@@ -11,9 +12,13 @@ public class OptionalApp {
     }
 
     public static void sayHello(String name) {
-        Optional<String> optionalName = Optional.ofNullable(name);
-        Optional<String> optionalUpperName = optionalName.map(value -> value.toUpperCase());
+        //Optional<String> optionalName = Optional.ofNullable(name);
+        //Optional<String> optionalUpperName = optionalName.map(value -> value.toUpperCase());
+        //optionalUpperName.ifPresent(value -> System.out.println("Hello " + value));
 
-        optionalUpperName.ifPresent(value -> System.out.println("Hello " + value));
+        Optional.ofNullable(name)
+                .map(String::toUpperCase)
+                .ifPresent(System.out::println);
+
     }
 }
